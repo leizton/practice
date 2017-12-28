43998e60fb418a5cbf88dbf1a3ee0f3504fcd57c
leizton <leizton@126.com>
2017-12-28 10:03:37 +0800

    libevent supported-method, config

--- cpp/event_base.cc
+++ cpp/event_base.cc
@@ -2,6 +2,35 @@
 
 #include <event2/event.h>
 
+void supportedMethod();
+event_config* getEventConfig();
+
 int main() {
+    supportedMethod();
+
+    event_config* cfg = getEventConfig();
+
     return 0;
+}
+
+void supportedMethod() {
+    const char** methods = event_get_supported_methods();
+    printf("supported methods: ");
+    for (int i = 0; methods[i] != NULL; ++i) {
+        printf("%s ", methods[i]);
+    }
+    printf("\n");
+}
+
+event_config* getEventConfig() {
+    event_config* cfg = event_config_new();
+    // if (cfg == NULL)
+
+    int ret = event_config_require_features(cfg,
+        event_method_feature::EV_FEATURE_ET |  // 边缘触发
+        event_method_feature::EV_FEATURE_O1 |  // 时间复杂度是O(1)
+        event_method_feature::EV_FEATURE_FDS   // 支持套接字外的fd
+    );
+
+    return cfg;
 }
--- cpp/run.sh
+++ cpp/run.sh
@@ -3,11 +3,7 @@ clear
 rm -f a.out
 
 include_dir=../include
-if [ $# -gt 0 ]; then
-    g++ -std=c++14 -I$include_dir $@ -lpthread
-else
-    g++ -std=c++14 -I$include_dir main.cc -lpthread
-fi
+g++ -std=c++14 -I$include_dir $@ -lpthread -levent
 
 if [ -f a.out ]; then
     ./a.out
