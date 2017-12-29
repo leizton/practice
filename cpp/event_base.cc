5119ddac19a8f26f00a18a004c88894abfcb0e4a
leizton <leizton@126.com>
2017-12-29 11:25:48 +0800

    ev_base

--- cpp/event_base.cc
+++ cpp/event_base.cc
@@ -1,6 +1,6 @@
-#include "common/header.h"
-
 #include <event2/event.h>
+
+#include "common/header.h"
 
 void supportedMethod();
 event_config* getEventConfig();
@@ -8,7 +8,9 @@ event_config* getEventConfig();
 int main() {
     supportedMethod();
 
-    event_config* cfg = getEventConfig();
+    event_config* ev_cfg = getEventConfig();
+    event_base* ev_base = event_base_new_with_config(ev_cfg);
+    printf("new event_base: %s\n", event_base_get_method(ev_base));
 
     return 0;
 }
@@ -27,10 +29,11 @@ event_config* getEventConfig() {
     // if (cfg == NULL)
 
     int ret = event_config_require_features(cfg,
-        event_method_feature::EV_FEATURE_ET |  // 边缘触发
-        event_method_feature::EV_FEATURE_O1 |  // 时间复杂度是O(1)
-        event_method_feature::EV_FEATURE_FDS   // 支持套接字外的fd
+        //event_method_feature::EV_FEATURE_ET |  // 边缘触发
+        //event_method_feature::EV_FEATURE_O1 |  // 激活一个事件的时间复杂度是O(1)
+        event_method_feature::EV_FEATURE_FDS  // 支持套接字外的fd
     );
 
+    event_config_set_flag(cfg, event_base_config_flag::EVENT_BASE_FLAG_NOLOCK);  // 不给event_base分配锁
     return cfg;
 }
