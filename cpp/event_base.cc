cae2d2f3a331e895cc896cdaa4a4fa85fde9fffa
leizton <leizton@126.com>
2018-01-03 09:22:09 +0800

    mod

--- cpp/event_base.cc
+++ cpp/event_base.cc
@@ -1,6 +1,7 @@
 #include <event2/event.h>
 
 #include "common/header.h"
+#include "common/log.h"
 
 void supportedMethod();
 event_config* getEventConfig();
@@ -10,16 +11,18 @@ int main() {
 
     event_config* ev_cfg = getEventConfig();
     event_base* ev_base = event_base_new_with_config(ev_cfg);
-    printf("new event_base: %s\n", event_base_get_method(ev_base));
+    LOG("new event_base: %s", event_base_get_method(ev_base));
+    event_config_free(ev_cfg);
 
+    event_base_free(ev_base);
     return 0;
 }
 
 void supportedMethod() {
     const char** methods = event_get_supported_methods();
-    printf("supported methods: ");
+    printf("supported methods:");
     for (int i = 0; methods[i] != NULL; ++i) {
-        printf("%s ", methods[i]);
+        printf(" %s", methods[i]);
     }
     printf("\n");
 }
@@ -28,12 +31,18 @@ event_config* getEventConfig() {
     event_config* cfg = event_config_new();
     // if (cfg == NULL)
 
-    int ret = event_config_require_features(cfg,
-        //event_method_feature::EV_FEATURE_ET |  // 边缘触发
-        //event_method_feature::EV_FEATURE_O1 |  // 激活一个事件的时间复杂度是O(1)
-        event_method_feature::EV_FEATURE_FDS  // 支持套接字外的fd
-    );
+    // 设置选择event_method时, 应该具有的特征
+    if (event_config_require_features(cfg,
+            // event_method_feature::EV_FEATURE_ET |  // 边缘触发
+            // event_method_feature::EV_FEATURE_O1 |  // 激活一个事件的时间复杂度是O(1)
+            event_method_feature::EV_FEATURE_FDS  // 支持套接字外的fd, poll-method
+        ) != 0) {
+        LOG("config method features fail");
+    }
 
-    event_config_set_flag(cfg, event_base_config_flag::EVENT_BASE_FLAG_NOLOCK);  // 不给event_base分配锁
+    // event_base的配置, event_base使用时不加锁
+    if (event_config_set_flag(cfg, event_base_config_flag::EVENT_BASE_FLAG_NOLOCK) != 0) {
+        LOG("config event_base flag fail");
+    }
     return cfg;
 }
