c840f4ccbea0984643f9f2f37d7cda17a7d96426
leizton <leizton@126.com>
2018-01-03 23:57:55 +0800

    timeval

--- cpp/sys_unistd.cc
+++ cpp/sys_unistd.cc
@@ -1,10 +1,22 @@
+#include <unistd.h>
 #include <sys/time.h>
 
 #include "common/log.h"
 
-int main() {
+void _timeval() {
     timeval tv = { 60, 1 };
     LOG("time: %ld.%06lds", tv.tv_sec, tv.tv_usec);
+
+    timeval start, end;
+    gettimeofday(&start, nullptr);
+    usleep(500);
+    gettimeofday(&end, nullptr);
+    LOG("sleep 500us, time diff: %ld.%06lds",
+        end.tv_sec - start.tv_sec, end.tv_usec - start.tv_usec);
+}
+
+int main() {
+    _timeval();
 
     return 0;
 }
