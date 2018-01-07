4f1a745dd3ebc752201e4d558268f82322b8abf4
leizton <leizton@126.com>
2018-01-07 16:28:11 +0800

    poll

--- cpp/poll.cc
+++ cpp/poll.cc
@@ -1,17 +1,21 @@
+#include <unistd.h>
 #include <sys/poll.h>
 
+#include "common/header.h"
 #include "common/log.h"
 
-const static int kBufSize = 100;
+const static int kBufSize = 8;
+
+void readWrapper(const int fd, char* const buf, int* const ridx, const int limit);
+void writeWrapper(const int fd, char* const buf, int* const widx, int* const ridx, const int limit);
 
 int main() {
     pollfd fds[2];
-    fds[0].fd = STDIN_FILENO,  fds[0].events = POLL_IN;
-    fds[1].fd = STDIN_FILEOUT, fds[1].events = POLL_OUT;
+    fds[0].fd = STDIN_FILENO,  fds[0].events = POLLIN;
+    fds[1].fd = STDOUT_FILENO, fds[1].events = POLLOUT;
 
     char buf[kBufSize];
-    int ridx, widx, rnum, wnum;
-    ridx = widx = 0;
+    int ridx = 0, widx = 0;
 
     for (;;) {
         int ret = poll(fds, 2, 1000);
@@ -20,21 +24,50 @@ int main() {
         } else if (ret == 0) {
             LOG("poll timeout");
         } else {
-            if ((fds[0].revents & POLL_IN) && ridx < kBufSize) {
-                rnum = read(fds[0].fd, buf + ridx, kBufSize - ridx);
-                if (rnum < 0) {
-                    LOGERR("read fail");
-                } else if (rnum > 0) {
-                    ridx += rnum;
-                }
+            if ((fds[0].revents & POLLIN)) {
+                readWrapper(fds[0].fd, buf, &ridx, kBufSize);
             }
-            if (fds[1].revents & POLL_OUT) {
-                if (widx < ridx) {
-                    wnum = write(fds[1].fd, buf + widx, ridx - widx);
-                }
+            if (fds[1].revents & POLLOUT) {
+                writeWrapper(fds[1].fd, buf, &widx, &ridx, kBufSize);
             }
         }
     }
 
     return 0;
+}
+
+void readWrapper(const int fd, char* const buf, int* const ridx, const int limit) {
+    if (*ridx < limit) {
+        int rnum = read(fd, buf + *ridx, limit - *ridx);
+        if (rnum > 0) {
+            *ridx += rnum;
+        } else if (rnum < 0) {
+            LOGERR("read fail. fd=%d", fd);
+        }
+    }
+}
+
+void writeWrapper(const int fd, char* const buf, int* const widx, int* const ridx, const int limit) {
+    int widx_t = *widx;
+    const int ridx_t = *ridx;
+    if (widx_t < ridx_t) {
+        int wnum = write(fd, buf + widx_t, ridx_t - widx_t);
+        if (wnum > 0) {
+            widx_t += wnum;
+            if (widx_t >= ridx_t) {
+                *widx = *ridx = 0;
+            } else if ((ridx_t - widx_t) >= (limit >> 1)) {
+                char* p = buf;
+                while (widx_t < ridx_t) {
+                    *p++ = buf[widx_t++];
+                }
+                *widx = 0;
+                *ridx = ridx_t - widx_t;
+            } else {
+                *widx = widx_t;
+            }
+        } else if (wnum < 0) {
+            LOGERR("write fail. fd=%d", fd);
+        }
+    }
 }
