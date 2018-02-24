e7383b491ed23bdcf9ec5112319a485f61b9bc8b
leizton <leizton@126.com>
2018-02-24 20:11:48 +0800

    format

--- cpp/construct.cc
+++ cpp/construct.cc
@@ -23,16 +23,10 @@ public:
         printf("con-1 %d\n", _fd);
     }
 
-    Resource(int _fd1, int _fd2)
-            : fd(-1),
-              ptr(nullptr, [_fd1, _fd2](void* p) { destory(_fd1); destory(_fd2); }) {
-        printf("con-2 %d, %d\n", _fd1, _fd2);
-    }
-
     // 通过new方式调用其他构造函数, 而不是直接调用其他构造函数
     // 直接调用Resource(x)是在创建一个临时对象
     Resource(double x) {
-        printf("con-3 %.2f\n", x);
+        printf("con-2 %.2f\n", x);
         new (this) Resource((int)x);
     }
 
@@ -82,6 +76,7 @@ public:
 
     void reset(int fd) {
         printf("resetting\n");
+        // output: con-1(fd), getRes, con&=(fd), close(old_fd), decon(old_fd)
         *this = getRes(fd);  // 调用移动赋值, 该语句结束后调用旧的*this的析构
         printf("reseted\n");
     }
@@ -92,13 +87,15 @@ void destory(int fd) {
 }
 
 Resource getRes(int fd) {
-    return Resource(fd);
+    Resource res(fd);
+    printf("getRes %d\n", fd);
+    return res;
 }
 
 void test1() {
     printf("\n>> A\n");
     {
-        // output: con-1
+        // output: con-1, getRes
         // 没有调用移动构造(con&&)
         Resource res = getRes(1);
         // output: decon, close
@@ -108,52 +105,45 @@ void test1() {
 
     printf("\n>> B\n");
     {
-        // output: con-2
-        Resource res(5,6);
-        // output: decon, close 5, close 6
-    }
-
-    printf("\n>> C\n");
-    {
-        // output: con-3, con-1
+        // output: con-2, con-1
         Resource res(2.53);
         // output: decon, close
     }
 
-    printf("\n>> D\n");
+    printf("\n>> C\n");
     {
         // output: con-1(1)
         Resource res(1);
-        // output: con-1(2), con&=
+        // output: resetting, ..., resetted
         res.reset(2);
-        // output: close(1), decon(1), reseted, decon(2), close(2)
+        // output: decon(2), close(2)
     }
 
-    printf("\n>> E\n");
+    printf("\n>> D\n");
     // output: con-1. 没有调用移动构造
     Resource resX0 = getRes(3);
     resX0.resName = "resX0";
 
-    printf("\n>> F\n");
+    printf("\n>> E\n");
     // output: con&
     // 此处不是调用赋值构造, 而是拷贝构造
     // 因为对象resX1还未创建, 不能调用赋值构造
     Resource resX1 = resX0;
     resX1.resName = "resX1";
 
-    printf("\n>> G\n");
+    printf("\n>> F\n");
     // output: con&
     Resource resX2(resX0);
     resX2.resName = "resX2";
 
-    printf("\n>> H\n");
+    printf("\n>> G\n");
     // output: con-0
     Resource resX3;
     // output: con=. 普通赋值
     resX3 = resX0;
     resX3.resName = "resX3";
 
-    printf("\n>> I\n");
+    printf("\n>> H\n");
     // output: decon(resX3), decon(resX2), decon(resX1), decon(resX0) close
     // 由于ptr是shared_ptr, 所以只close一次
 }
