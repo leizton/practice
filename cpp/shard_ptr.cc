5e48387b0fe010cd813cf01120fc402b5ab7ccac
leizton <leizton@126.com>
2018-11-03 13:26:09 +0800

    shared_ptr

--- cpp/shard_ptr.cc
+++ cpp/shard_ptr.cc
@@ -3,6 +3,29 @@
 
 using namespace std;
 
+#define RUN base
+
+struct Foo;
+
+// make_shared<T>(Args&&... args)
+// shared_ptr的copy-assign和assign
+// 自身赋值
+void base() {
+  auto p1 = make_shared<Foo>(10);
+  auto p2 = p1;
+  auto p3(p2);
+  cout << p1.use_count() << endl;  // 3
+
+  {
+    auto p4 = p3;
+    cout << p1.use_count() << endl; // 4
+  }
+  cout << p1.use_count() << endl; // 3
+
+  p1 = p1;  // operator= 里判断 this== &another
+  cout << p1.use_count() << endl;  // 3
+}
+
 // 管理资源. 利用析构函数释放资源
 void manageRes() {
   int v = 0;
@@ -14,5 +37,13 @@ void manageRes() {
 }
 
 int main() {
-  manageRes();
+  RUN();
 }
+
+struct Foo {
+  Foo() : v_(0) { cout << "Foo-con v=0" << endl; }
+  Foo(int v) : v_(v) { cout << "Foo-con v=" << v << endl; }
+  ~Foo() { cout << "Foo-decon" << endl; }
+
+  int v_;
+};
