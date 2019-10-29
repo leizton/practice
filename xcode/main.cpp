6f5e92572127b79c1d8139a6d05e8a0b88c370e0
leizton <leizton@126.com>
2019-10-29 12:07:26 +0800

    mod

--- xcode/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "001.hpp"
-#include "solu.hpp"
-
-int main(int argc, const char* argv[]) {
-  p001::test();
-
-  Solution::test();
-}
--- xcode/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -f a.out
-g++ -std=c++14 main.cpp
-if [ -f a.out ]; then
-  ./a.out
-fi
