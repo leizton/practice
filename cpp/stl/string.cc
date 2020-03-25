cadb1264a83d77a0aa51166f06c1317a62f58614
leizton <leizton@126.com>
2020-03-25 16:58:01 +0800

    string find rfind

--- cpp/stl/string.cc
+++ cpp/stl/string.cc
@@ -1,27 +1,24 @@
-#include <iostream>
-#include <stdarg.h>
+#include "util/base.h"
 
-#include "common/header.h"
 
-void split() {
-  string str = "1,2,3";
-  stringstream ss(str);
-  for (string e; getline(ss, e, ','); ) {
-    cout << e << endl;
-  }
+def(find_rfind) {
+  string s = "a/b/c/d";
+  assert_eq(std::string::npos, s.find('\\'));
+  assert_eq(1, s.find('/'));
+  assert_eq(3, s.find('/', 2));
+  assert_eq(5, s.rfind('/'));
 }
 
-string format(const char* fmt, ...) {
-  static const int bufsize = 32;
-  char buf[bufsize];
-  va_list ap;
-  va_start(ap, fmt);
-  vsnprintf(buf, bufsize, fmt, ap);
-  va_end(ap);
-  return string(buf);
+
+def(dirpath) {
+  auto getDirpath = [] (const string& path) -> string {
+    auto idx = path.rfind('/');
+    return (idx == std::string::npos) ? "" : path.substr(0, idx+1);
+  };
+  assert_eq("/home/", getDirpath("/home/a.txt"));
+  assert_eq("/home/lz/", getDirpath("/home/lz/a.txt"));
+  assert_eq("", getDirpath("a.txt"));
 }
 
-int main() {
-  // split();
-  // cout << format("test-%d-%s", 17, "abc") << endl;
-}
+
+main_run;
