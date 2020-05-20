c75efcf9cf97c4340c441ca150bd581a83b05e17
leizton <leizton@126.com>
2020-05-20 19:50:13 +0800

    fmt

--- cpp/util/count_down_latch.h
+++ cpp/util/count_down_latch.h
@@ -6,7 +6,16 @@
 
 class CountDownLatch {
 public:
+  CountDownLatch() : num_(0) {}
   CountDownLatch(int32_t n) : num_(n) {}
+
+  void reset(int32_t n) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    num_ = n;
+    if (num_ <= 0) {
+      cond_.notify_all();
+    }
+  }
 
   void wait() {
     std::unique_lock<std::mutex> lk(mtx_);
@@ -15,10 +24,6 @@ public:
 
   void countDown(int32_t n) {
     std::lock_guard<std::mutex> lk(mtx_);
-    if (num_ <= 0) {
-      cond_.notify_all();
-      return;
-    }
     num_ -= n;
     if (num_ <= 0) {
       cond_.notify_all();
