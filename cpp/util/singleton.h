2b55c2287451fbc3bc2d71ad68de6ff9c4f35794
leizton <leizton@126.com>
2020-10-20 14:22:13 +0800

    singleton

--- cpp/util/singleton.h
+++ cpp/util/singleton.h
@@ -1,25 +1,25 @@
+#include <memory>
+
+/*
+
 template<class T>
 class Singleton {
 private:
   class InstanceHolder {
   public:
-    static T* instance_;
+    std::unique_ptr<T> instance_;
 
     InstanceHolder() {
-      instance_ = new T;
+      instance_.reset(new T);
     }
 
-    ~InstanceHolder() {
-      if (instance_) {
-        delete instance_;
-        instance_ = nullptr;
-      }
+    ~InstanceHolder() {  // 程序退出时析构InstanceHolder，进而析构T
     }
   };
   static InstanceHolder holder_;
 
 public:
-  static T& instance() {
+  static T& instance() {  // 引用静态的holder_
     return *holder_.instance_;
   }
 
@@ -31,7 +31,41 @@ private:
 };
 
 template<class T>
-T* Singleton<T>::InstanceHolder::instance_ = nullptr;
+typename Singleton<T>::InstanceHolder Singleton<T>::holder_;  // 因为是模板，所以static成员变量的定义写在.h而不是.cc里
+
+# example
+
+// test.h
+typedef Singleton<Test> TestSingleton;
+
+// main.cc
+Test& t = TestSingleton::instance();
+
+*/
+
 
 template<class T>
-typename Singleton<T>::InstanceHolder Singleton<T>::holder_;
+class Singleton {
+public:
+  static T& instance() {
+    static const std::unique_ptr<T> instance_{new T};
+    return *instance_;
+  }
+
+protected:
+  Singleton() {}
+  virtual ~Singleton() {}
+  Singleton(const Singleton&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+};
+
+/*
+
+# example
+
+class Test : public Singleton<Test> {
+};
+
+Test& t = Test::instance();
+
+*/
