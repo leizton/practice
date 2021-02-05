97691bf4ea116879b74621c787b68fccaa42601c
leizton <leizton@126.com>
2021-02-05 13:01:31 +0800

    bits/stdc++.h

--- cpp/util/cpp_lib.h
+++ cpp/util/cpp_lib.h
@@ -1,46 +1,11 @@
 #pragma once
 
-#include <cstdlib>
-#include <cstdarg>
-
-#include <cstdio>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-#include <algorithm>
-#include <bitset>
-#include <string>
-#include <vector>
-#include <list>
-#include <queue>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-
-#include <thread>
-#include <atomic>
-#include <mutex>
-#include <condition_variable>
-#include <future>
-
-#include <cstdlib>
-#include <utility>
-#include <exception>
-#include <type_traits>
-#include <typeinfo>
-#include <memory>
-#include <cstring>
-#include <cmath>
-#include <ctime>
-#include <chrono>
+#include <bits/stdc++.h>
 
 /*
 #include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 //*/
 
 using namespace std;
