b747e6b2d28959748777a1bb23f4e7677e9eac4c
leizton <leizton@126.com>
2021-04-09 15:39:50 +0800

    double_buffer

--- cpp/test/test_double_buffer.cc
+++ cpp/test/test_double_buffer.cc
@@ -5,9 +5,9 @@ int main() {
 
   auto* ina_obj = buf.getInactiveObj();
   *ina_obj = "a";
-  assert_eq(1u, buf.getActiveIndex());
-  buf.switchObj();
   assert_eq(0u, buf.getActiveIndex());
+  buf.switchObj();
+  assert_eq(1u, buf.getActiveIndex());
 
   assert_eq(0u, buf.getActiveUseCount());
   assert_eq(std::string("a"), *buf.getActiveObj());
@@ -23,25 +23,21 @@ int main() {
   obj_1.reset();
   assert_eq(1u, buf.getActiveUseCount());
 
-  std::atomic<bool> change_done = false;
-
-  auto change = [&buf, &change_done]() {
+  auto update_fn = [&buf]() {
     auto* ina_obj = buf.getInactiveObj();
     *ina_obj = "b";
     buf.switchObj();
-    change_done = true;
   };
-  std::thread th(change);
 
-  sleepSec(1);
+  assert_eq(1u, buf.getActiveIndex());
+  std::thread update_th(update_fn);
+  update_th.join();
   assert_eq(0u, buf.getActiveIndex());
 
-  assert_F(change_done); // happens before change-thread complete
-  obj_2.reset();
-  assert_eq(0u, buf.getActiveUseCount());
-
-  th.join();
-  assert_T(change_done);
-  assert_eq(1u, buf.getActiveIndex());
+  assert_eq(std::string("a"), *obj_2);
   assert_eq(std::string("b"), *buf.getActiveObj());
+
+  assert_eq(1u, buf.getInactiveUseCount());
+  obj_2.reset();
+  assert_eq(0u, buf.getInactiveUseCount());
 }
--- cpp/util/double_buffer.h
+++ cpp/util/double_buffer.h
@@ -3,93 +3,105 @@
 #include <atomic>
 #include <chrono>
 
+/**
+ * About the implement, the operator of switch & getActive is lockless.
+ * However, getActive may return different instances at the same time.
+ */
 template <class DataType>
 class DoubleBuffer final {
-private:
-  static const int MaskShift = 31;
-  static const uint32_t Mask = (1u << MaskShift);
-
 public:
-  DoubleBuffer(int switch_sleep_us = 100);
+  DoubleBuffer();
   ~DoubleBuffer();
   DoubleBuffer(const DoubleBuffer&) = delete;
   DoubleBuffer& operator=(const DoubleBuffer&) = delete;
 
   std::shared_ptr<DataType> getActiveObj(); // for read
 
+  /**
+   * The update-procedure as follow:
+   *   1. LOCK
+   *   2. T* inactive_obj = getInactiveObj()
+   *   3. modify(inactive_obj)
+   *   4. switchObj()
+   *   5. UNLOCK
+   * update-procedure must be protected by mutex
+   */
   DataType* getInactiveObj(); // for write
-
   void switchObj();
 
+  uint32_t getActiveIndex() const { return active_idx_.load(); }
+  uint32_t getInactiveIndex() const { return 1u - getActiveIndex(); }
+
   uint32_t getActiveUseCount() const;
-  uint32_t getActiveIndex() const;
-  uint32_t getInactiveIndex() const;
+  uint32_t getInactiveUseCount() const;
 
 private:
+  struct Counter {
+    Counter() : cnt(0) {}
+    std::atomic<uint32_t> cnt;
+    // wait cnt return to zero
+    void waitZero() {
+      while (cnt.load() > 0u) {
+        std::this_thread::sleep_for(std::chrono::duration<int, std::micro>(100));
+      }
+    }
+  };
+
   struct Deleter {
-    Deleter(std::atomic<uint32_t>& cnt) : cnt_(cnt) {}
-    void operator()(DataType*) { cnt_--; }
-    std::atomic<uint32_t>& cnt_;
+    void operator()(DataType*) { (*cnt)--; }
+    std::atomic<uint32_t>* cnt;
   };
 
 private:
-  const int switch_sleep_us_;
-  DataType* buf_[2];
   std::atomic<uint32_t> active_idx_;
-  Deleter deleter_;
+  DataType* buf_[2];
+  Counter counter_[2];
+  Deleter deleter_[2];
 };
 
 template <class DataType>
-DoubleBuffer<DataType>::DoubleBuffer(int switch_sleep_us)
-    : switch_sleep_us_(switch_sleep_us)
-    , deleter_(active_idx_) {
+DoubleBuffer<DataType>::DoubleBuffer()
+    : active_idx_(0) {
   buf_[0] = new DataType;
   buf_[1] = new DataType;
-  active_idx_ = Mask;
+  deleter_[0].cnt = &counter_[0].cnt;
+  deleter_[1].cnt = &counter_[1].cnt;
 }
 
 template <class DataType>
 DoubleBuffer<DataType>::~DoubleBuffer() {
-  delete buf_[0];
-  buf_[0] = nullptr;
-  delete buf_[1];
-  buf_[1] = nullptr;
-}
-
-template <class DataType>
-std::shared_ptr<DataType> DoubleBuffer<DataType>::getActiveObj() {
-  uint32_t idx = ((++active_idx_) & Mask) >> MaskShift;
-  return std::shared_ptr<DataType>(buf_[idx], deleter_);
-}
-
-template <class DataType>
-DataType* DoubleBuffer<DataType>::getInactiveObj() {
-  uint32_t idx = getActiveIndex();
-  return buf_[1u - idx];
-}
-
-template <class DataType>
-void DoubleBuffer<DataType>::switchObj() {
-  uint32_t expect = active_idx_.load() & Mask;
-  const uint32_t val = expect ^ Mask;
-  while (!active_idx_.compare_exchange_strong(expect, val)) {
-    // TODO: 此处有活锁
-    expect = active_idx_.load() & Mask;
-    std::this_thread::sleep_for(std::chrono::duration<int, std::micro>(switch_sleep_us_));
+  for (int i = 0; i < 2; i++) {
+    counter_[i].waitZero();
+    delete buf_[i];
+    buf_[i] = nullptr;
   }
 }
 
 template <class DataType>
+std::shared_ptr<DataType> DoubleBuffer<DataType>::getActiveObj() {
+  const uint32_t i = getActiveIndex();
+  counter_[i].cnt++;
+  return std::shared_ptr<DataType>(buf_[i], deleter_[i]);
+}
+
+template <class DataType>
+DataType* DoubleBuffer<DataType>::getInactiveObj() {
+  const uint32_t i = getInactiveIndex();
+  counter_[i].waitZero();
+  return buf_[i];
+}
+
+template <class DataType>
+void DoubleBuffer<DataType>::switchObj() {
+  active_idx_.store(getInactiveIndex());
+}
+
+template <class DataType>
 uint32_t DoubleBuffer<DataType>::getActiveUseCount() const {
-  return active_idx_.load() & (~Mask);
+  return counter_[getActiveIndex()].cnt.load();
 }
 
 template <class DataType>
-uint32_t DoubleBuffer<DataType>::getActiveIndex() const {
-  return (active_idx_.load() & Mask) >> MaskShift;
-}
-
-template <class DataType>
-uint32_t DoubleBuffer<DataType>::getInactiveIndex() const {
-  return 1u - getActiveIndex();
+uint32_t DoubleBuffer<DataType>::getInactiveUseCount() const {
+  return counter_[getInactiveIndex()].cnt.load();
 }
