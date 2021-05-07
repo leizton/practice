a7e69e2988f18708b7d7e7aa5b23eb58a64bb613
leizton <leizton@126.com>
2021-05-07 11:44:28 +0800

    mod

--- cpp/stl/adetails.cc
+++ cpp/stl/adetails.cc
@@ -7,13 +7,13 @@ struct Sleeper {
   }
 
   ~Sleeper() {
-    println("~Sleeper{", create_thread_id, "}");
+    LOG() << "~Sleeper{" << create_thread_id << "}";
   }
 
   uint64_t create_thread_id;
 };
 
-def(static_instance_in_func) {
+run(static_instance_in_func) {
   /*
     从 c++11 开始, sleeper_的构造是线程安全
     https://stackoverflow.com/questions/1661529/is-meyers-implementation-of-the-singleton-pattern-thread-safe
@@ -21,23 +21,25 @@ def(static_instance_in_func) {
     根据 c++11标准 section 6.7. http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3337.pdf
     “the concurrent execution shall wait for completion of the initialization”
   */
+
+  srand(time(NULL));
+
   auto instance = []() -> Sleeper& {
+    sleepMs(100 + (rand() % 500));
     static Sleeper sleeper_(5);
     return sleeper_;
   };
 
-  std::set<uint64_t> thread_ids;
+  std::unordered_map<uint64_t, int> thread_ids;
   for (int i = 0; i < 10; i++) {
     std::thread th(instance);
+    thread_ids.emplace(getThreadId(th), i);
     th.detach();
-    thread_ids.insert(getThreadId(th));
   }
-  sleepSec(1);
 
-  println("main thread: ", currentThreadId());
-  println("create sleeper thread: ", instance().create_thread_id);
-  println("create sleeper thread: ", instance().create_thread_id);
-  println(thread_ids.find(instance().create_thread_id) != thread_ids.end());
+  const uint64_t create_thread_id = instance().create_thread_id; // 会等创建线程结束
+  LOG() << "createSleeper-th: " << create_thread_id;
+  LOG() << "create thread index: " << thread_ids[create_thread_id];
 }
 
 main_run;
