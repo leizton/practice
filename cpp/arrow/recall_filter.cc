120afbf615f5c4a6decbf87a5011abfe4a0739c7
leizton <leizton@126.com>
2021-06-15 15:57:32 +0800

    update recall_filter

--- cpp/arrow/recall_filter.cc
+++ cpp/arrow/recall_filter.cc
@@ -42,7 +42,7 @@ int main() {
   ChunkArrayIter<uint64_t> out_num_it(table->GetColumnByName("out_num"));
 
   // stat
-  std::unordered_map<BinaryData, std::unordered_map<uint64_t, RecoStatItem>> reco_stat;
+  std::unordered_map<BinaryData /* gh */, std::unordered_map<uint64_t /* reco_id */, RecoStatItem>> reco_stat;
   while (gh_it.not_end()) {
     auto gh = gh_it.next();
     auto recoid = recoid_it.next();
@@ -50,9 +50,36 @@ int main() {
     reco_st.recall_num += recall_num_it.next();
     reco_st.out_num += out_num_it.next();
   }
-  std::unordered_map<BinaryData, GhStatItem> gh_stat;
+
+  std::map<std::string /* gh */, std::vector<double>> recall_filter_ratio_stat;
   for (auto& gh_2_recos : reco_stat) {
-    auto& gh_st = gh_stat[gh_2_recos.first];
+    std::string gh_name(gh_2_recos.first.data, gh_2_recos.first.len);
+    auto& ratios = recall_filter_ratio_stat[gh_name];
+    ratios.push_back(0.);
+    double ratio_sum = 0.;
+    for (auto& reco_st : gh_2_recos.second) {
+      if (reco_st.second.out_num <= 1) continue;
+      double ratio = (double)reco_st.second.recall_num / (double)reco_st.second.out_num;
+      ratios.push_back(ratio);
+      ratio_sum += ratio;
+    }
+    std::sort(ratios.begin(), ratios.end());
+    ratios[0] = ratio_sum / (double)(ratios.size() - 1);
+  }
+  for (auto& e : recall_filter_ratio_stat) {
+    auto& ratios = e.second;
+    int num = (int)ratios.size();
+    LOG() << e.first
+          << ", avg=" << setiosflags(ios::fixed) << setprecision(2) << ratios[0]
+          << ",  p50=" << setiosflags(ios::fixed) << setprecision(2) << ratios[(num >> 1)]
+          << ",  p90=" << setiosflags(ios::fixed) << setprecision(2) << ratios[int(num * 0.9)]
+          << ",  max=" << setiosflags(ios::fixed) << setprecision(2) << ratios[(num - 1)];
+  }
+
+  std::map<std::string /* gh */, GhStatItem> gh_stat;
+  for (auto& gh_2_recos : reco_stat) {
+    std::string gh_name(gh_2_recos.first.data, gh_2_recos.first.len);
+    auto& gh_st = gh_stat[gh_name];
     for (auto& reco_st : gh_2_recos.second) {
       gh_st.recall_num_avg += reco_st.second.recall_num;
       gh_st.recall_num_max = std::max(gh_st.recall_num_max, reco_st.second.recall_num);
@@ -66,21 +93,30 @@ int main() {
     gh_st.second.out_num_avg /= gh_st.second.reco_num;
   }
   for (auto& gh_st : gh_stat) {
-    LOG() << std::string(gh_st.first.data, gh_st.first.len)
+    LOG() << gh_st.first
           << ", recall_num=[" << gh_st.second.recall_num_avg << ", " << gh_st.second.recall_num_max << "]"
           << ", out_num=[" << gh_st.second.out_num_avg << ", " << gh_st.second.out_num_max << "]"
           << ", reco_num=" << gh_st.second.reco_num;
   }
 
   /*
-  Immersive, recall_num=[30740, 75614], out_num=[2301, 3615], reco_num=334
-  VideoFeed, recall_num=[11923, 33344], out_num=[2531, 7407], reco_num=563
-  VideoOffline, recall_num=[29990, 56547], out_num=[4251, 9001], reco_num=2689
-  Radio, recall_num=[3631, 31775], out_num=[2057, 5954], reco_num=168
-  CommunityFeed, recall_num=[6909, 6914], out_num=[1830, 2166], reco_num=42
-  ZLeaf, recall_num=[18320, 51148], out_num=[3676, 10048], reco_num=2689
-  ChannelFeed, recall_num=[3790, 9489], out_num=[1537, 2880], reco_num=277
-  FullScreenFeed, recall_num=[30238, 82537], out_num=[2042, 4110], reco_num=1281
+  ChannelFeed,    avg=2.73,   p50=2.16,   p90=4.17,   max=25.02
+  CommunityFeed,  avg=3.93,   p50=3.21,   p90=5.19,   max=5.22
+  FullScreenFeed, avg=15.41,  p50=15.08,  p90=18.67,  max=41.32
+  Immersive,      avg=13.28,  p50=13.05,  p90=16.29,  max=23.05
+  Radio,          avg=1.44,   p50=1.01,   p90=2.16,   max=7.42
+  VideoFeed,      avg=4.75,   p50=5.11,   p90=5.39,   max=8.24
+  VideoOffline,   avg=7.57,   p50=7.05,   p90=9.73,   max=57.37
+  ZLeaf,          avg=5.12,   p50=5.05,   p90=6.90,   max=13.01
+
+  ChannelFeed,    recall_num=[3790,   9489],   out_num=[1537,  2880],  reco_num=277
+  CommunityFeed,  recall_num=[6909,   6914],   out_num=[1830,  2166],  reco_num=42
+  FullScreenFeed, recall_num=[30238, 82537],   out_num=[2042,  4110],  reco_num=1281
+  Immersive,      recall_num=[30740, 75614],   out_num=[2301,  3615],  reco_num=334
+  Radio,          recall_num=[3631,  31775],   out_num=[2057,  5954],  reco_num=168
+  VideoFeed,      recall_num=[11923, 33344],   out_num=[2531,  7407],  reco_num=563
+  VideoOffline,   recall_num=[29990, 56547],   out_num=[4251,  9001],  reco_num=2689
+  ZLeaf,          recall_num=[18320, 51148],   out_num=[3676, 10048],  reco_num=2689
   */
 
   return 0;
