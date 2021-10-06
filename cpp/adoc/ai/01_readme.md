3597c535a16a2b8658b3c86701dfcac52f8baf9a
leizton <leizton@126.com>
2021-10-07 00:55:39 +0800

    NLP: L-PCFG

--- cpp/adoc/ai/01_readme.md
+++ cpp/adoc/ai/01_readme.md
@@ -1,5 +1,11 @@
 # ref
 paper search: https://paperswithcode.com/sota
 
-# 名词
+# 概念
 SOTA：state of the art 当前技术状态，SOTA模型指当前最好的模型
+generative model：生成模型
+discriminative model：判别模型
+  监督学习方法分成生成方法和判别方法，所学到的模型分别是生成模型和判别模型
+  GM是先学习联合概率P(XY)，然后用P(Y|X)=P(XY)/P(X)得到条件概率，例如贝叶斯
+  DM是直接构建P(Y|X)来预测y，例如线性回归、SVM、随机森林、CRF(条件随机场)
+MLE：maximum-likelihood estimates 最大似然估计
--- cpp/nlp/spacy_demo.py
+++ cpp/nlp/spacy_demo.py
@@ -4,5 +4,6 @@ from spacy import displacy
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("We present a novel generative model for natural language tree structures in which semantic structures are scored with separate models")
 doc = nlp("he says that you like books")
+doc = nlp("The assumption that complements are generated independently of one another often leads to incorrect parses")
 
 displacy.serve(doc, style='dep', options={'compact':True})
