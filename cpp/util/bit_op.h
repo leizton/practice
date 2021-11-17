ffe19a9762308ae85f2ca1dfe001b4e79c77f592
leizton <leizton@126.com>
2021-11-17 15:10:49 +0800

    getFirstNonZeroBitPos __builtin_clzll

--- cpp/util/bit_op.h
+++ cpp/util/bit_op.h
@@ -1,6 +1,10 @@
 #pragma once
 
 inline int getFirstNonZeroBitPos(uint64_t x) {
+  return 63 - __builtin_clzll(x);
+}
+
+inline int getFirstNonZeroBitPos_v1(uint64_t x) {
   static const int tb[67] = {
       -1, 0, 1, 39, 2, 15, 40, 23, 3, 12, 16, 59, 41, 19, 24, 54,
       4, 0, 13, 10, 17, 62, 60, 28, 42, 30, 20, 51, 25, 44, 55, 47,
@@ -11,7 +15,27 @@ inline int getFirstNonZeroBitPos(uint64_t x) {
   return tb[(x % 67)];
 }
 
-inline int getFirstNonZeroBitPos_v1(uint64_t x) {
+inline int getFirstNonZeroBitPos_v2(uint64_t v) {
+  static const int MultiplyDeBruijnBitPosition[64] = {
+      63, 0, 58, 1, 59, 47, 53, 2,
+      60, 39, 48, 27, 54, 33, 42, 3,
+      61, 51, 37, 40, 49, 18, 28, 20,
+      55, 30, 34, 11, 43, 14, 22, 4,
+      62, 57, 46, 52, 38, 26, 32, 41,
+      50, 36, 17, 19, 29, 10, 13, 21,
+      56, 45, 25, 31, 35, 16, 9, 12,
+      44, 24, 15, 8, 23, 7, 6, 5};
+  v |= v >> 1;
+  v |= v >> 2;
+  v |= v >> 4;
+  v |= v >> 8;
+  v |= v >> 16;
+  v |= v >> 32;
+  v -= v >> 1;
+  return MultiplyDeBruijnBitPosition[(v * 0x07EDD5E59A4E28C2ULL) >> 58];
+}
+
+inline int getFirstNonZeroBitPos_v3(uint64_t x) {
   static const uint64_t flag1 = 0xFFFFFFFF;
   static const uint64_t flag2 = 0xFFFF;
   static const uint64_t flag3 = 0xFF;
