4cfa88d7c635b4beeaac613fad195114ae4fe662
leizton <leizton@126.com>
2022-02-21 01:29:49 +0800

    rotate array

--- cpp/code/双指针/0189_rotate_array.cc
+++ cpp/code/双指针/0189_rotate_array.cc
@@ -3,7 +3,7 @@
 void rotate(vector<int>& nums, int k) {
   k = k % ((int)nums.size());
   if (k == 0) return;
-  auto reverse = [](vector<int>& nums, int left, int right) {
+  static auto reverse = [](vector<int>& nums, int left, int right) {
     while (left < right) {
       int tmp = nums[left];
       nums[left] = nums[right];
@@ -15,4 +15,33 @@ void rotate(vector<int>& nums, int k) {
   reverse(nums, 0, (int)nums.size()-1);
   reverse(nums, 0, k-1);
   reverse(nums, k, (int)nums.size()-1);
+}
+
+void rotate(vector<int>& nums, int k) {
+  static auto gcd = [](int n, int k) {
+    if (n < k) std::swap(n, k);
+    if (k == 0) return 0;
+    for (int t; k != 0; ) {
+      t = n;
+      n = k;
+      k = t % k;
+    }
+    return n;
+  };
+  int n = (int)nums.size();
+  k = k % n;
+  if (k == 0) return;
+  int G = gcd(n, k);
+  int b = n / G;
+  for (int i = 0; i < G; i++) {
+    int prev = nums[i];
+    int curr = i + k;
+    for (int j = 1; j < b; j++) {
+      int tmp = nums[curr % n];
+      nums[curr % n] = prev;
+      prev = tmp;
+      curr += k;
+    }
+    nums[i] = prev;
+  }
 }
