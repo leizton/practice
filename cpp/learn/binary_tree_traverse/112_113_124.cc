7ad789b2213baae1deb3fefe8b2e9f553394d7a4
leizton <leizton@126.com>
2022-09-07 23:28:32 +0800

    binary_tree_traverse 124

--- cpp/learn/binary_tree_traverse/112_113_124.cc
+++ cpp/learn/binary_tree_traverse/112_113_124.cc
@@ -1,6 +1,8 @@
 #include <header>
 
+
 /*
+112.cc
 给你二叉树的根节点 root 和一个表示目标和的整数 target
 判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加的和等于目标值 target
 如果存在，返回 true ；否则，返回 false
@@ -12,7 +14,9 @@ bool hasPathSum(TreeNode* root, int target) {
   return hasPathSum(root->left, target) || hasPathSum(root->right, target);
 }
 
+
 /*
+113.cc
 给你二叉树的根节点 root 和一个整数目标和 target
 找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径
 */
@@ -42,5 +46,34 @@ vector<vector<int>> pathSum(TreeNode* root, int target) {
   if (root == nullptr) return ans;
   vector<int> curr_path;
   pathSumAid(root, target, curr_path, ans);
+  return ans;
+}
+
+
+/*
+124.cc
+路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列
+同一个节点在一条路径序列中至多出现一次 。该路径至少包含一个节点，可以不经过根节点
+路径和是路径中各节点值的总和
+给你一个二叉树的根节点 root，返回其最大路径和
+*/
+// 返回以根节点为终点的路径的最大值
+int traverse(TreeNode* root, int& ans) {
+  if (root->left == nullptr && root->right == nullptr) {
+    ans = std::max(ans, root->val);
+    return root->val;
+  }
+  int l = (root->left) ? traverse(root->left, ans) : INT32_MIN;
+  int r = (root->right) ? traverse(root->right, ans) : INT32_MIN;
+  // 4种可能: root, left+root, root+right, left+root+right
+  if (l < 0) l = 0;
+  if (r < 0) r = 0;
+  ans = std::max(ans, l + root->val + r);
+  return std::max(l, r) + root->val;
+}
+int maxPathSum(TreeNode* root) {
+  if (root == nullptr) return 0;
+  int ans = INT32_MIN;
+  traverse(root, ans);
   return ans;
 }
