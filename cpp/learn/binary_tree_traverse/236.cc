abfd4e7e6571ad7dab7e8ab91e7d47984ba710e4
leizton <leizton@126.com>
2022-09-13 09:22:47 +0800

    mod

--- cpp/learn/binary_tree_traverse/236.cc
+++ cpp/learn/binary_tree_traverse/236.cc
@@ -2,6 +2,11 @@
 
 /*
 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+比较简单的思路是前序遍历树分别得出 从root到p和q的 路径vector<Node*> path_p path_q
+找出path_p和path_q的最后一个公共节点
+
+如果树的节点可以做标记，那就找到p后回溯时做标记，找到q后回溯时第一个有标记的节点就是LCA
 */
 
 int findLowestCommonAncestor(TreeNode* node, TreeNode* p, TreeNode* q, TreeNode*& answer) {
@@ -35,8 +40,7 @@ int findLowestCommonAncestor(TreeNode* node, TreeNode* p, TreeNode* q, TreeNode*
     return mark;
   }
 }
-
-TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+TreeNode* lowestCommonAncestorV0(TreeNode* root, TreeNode* p, TreeNode* q) {
   if (p == q || p == nullptr || q == nullptr) return (p == nullptr) ? q : p;
   if (root == nullptr || root == p || root == q) root;
   TreeNode* answer = nullptr;
@@ -44,13 +48,29 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
   return answer;
 }
 
-/**
- * 后序遍历
- */
-TreeNode* lowestCommonAncestorV1(TreeNode* root, TreeNode* p, TreeNode* q) {
-  if (!root || root == p || root == q) return root;
+/*
+  隐含前提: p q 都在树中
+
+  在当前根节点root的树中同时 search p q, 有4种可能情况
+  ① 没有找到 p q, 返回NULL
+  ② 找到 p q 中的某一个, 返回p或q 标记找到, 通知上层节点
+  ③ 找到 p q
+
+  对于情况③
+    if 在左右子树分别找到 p q
+      root 是 LCA, 返回root
+    elif 在左子树找到 p q
+      左子树包含 LCA, 返回左子树的搜索结果
+    else 在右子树找到 p q
+      右子树包含 LCA, 返回右子树的搜索结果
+*/
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+  if (!root) return nullptr;  // 情况①
+  if (root == p) return p;    // 情况②
+  if (root == q) return q;    // 情况②
   TreeNode *left = lowestCommonAncestor(root->left, p, q);
   TreeNode *right = lowestCommonAncestor(root->right, p, q);
-  if (left && right) return root;  // p q 在不同子树
-  return left ? left : right;      // p q 在相同子树
+  if (!left && !right) return nullptr;  // 情况①
+  if (left && right)   return root;     // 情况③
+  return left ? left : right;           // 情况② ③
 }
