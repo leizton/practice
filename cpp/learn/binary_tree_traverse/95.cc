dcd22a843fbeeb660df500c1902f2760f3925dda
leizton <leizton@126.com>
2022-09-25 01:19:53 +0800

    binary_tree_traverse 95

---
+++ cpp/learn/binary_tree_traverse/95.cc
@@ -0,0 +1,39 @@
+#include <header>
+
+/*
+
+给定一个整数 n
+
+请你生成并返回所有可能的不同二叉搜索树
+这些二叉搜索树都包含n个节点，且节点值从1到n
+
+*/
+
+void dfs(int from, int to, vector<TreeNode*>& ret) {
+  vector<TreeNode*> left_trees, right_trees;
+  for (int root_val = from; root_val <= to; root_val++) {
+    dfs(from, root_val-1, left_trees);
+    dfs(root_val+1, to, right_trees);
+    if (left_trees.empty()) left_trees.push_back(nullptr);
+    if (right_trees.empty()) right_trees.push_back(nullptr);
+    for (auto* l : left_trees) {
+      for (auto* r : right_trees) {
+        ret.push_back(new TreeNode(root_val, l, r));
+      }
+    }
+    left_trees.clear();
+    right_trees.clear();
+  }
+}
+vector<TreeNode*> generateTrees(int n) {
+  vector<TreeNode*> ret;
+  dfs(1, n, ret);
+  return ret;
+}
+
+int main() {
+  auto ret = generateTrees(3);
+  for (auto* root : ret) {
+    print(CmdReader::dumpTree(root));
+  }
+}
