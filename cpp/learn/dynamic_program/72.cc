3583d051b425b689d08fd89735226bbaf4f87494
leizton <leizton@126.com>
2024-03-13 19:11:37 +0800

    compile

--- cpp/learn/dynamic_program/72.cc
+++ cpp/learn/dynamic_program/72.cc
@@ -9,29 +9,24 @@
   - 改一个字符
 */
 
-int minDistance(string p, string s) {
-  const int m = p.length(), n = s.length();
+int minDistance(string s, string t) {
+  int m = (int)s.size(), n = (int)t.size();
+  if (m == 0 || n == 0) return max(m, n);
 
-  // dp[i][j]: p[0:i-1]变成s[0:j-1]的最小编辑距离
-  vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-  // s是空串
-  for (int j = 1; j <= n; j++) {
-    dp[0][j] = j;
+  vector<vector<int>> dp(m, vector<int>(n, 0));
+  dp[0][0] = (s[0] == t[0]) ? 0 : 1;
+
+  for (int i = 1; i < m; i++) {
+    dp[i][0] = (s[i] == t[0]) ? i : (dp[i-1][0] + 1);
   }
-  // t是空串
-  for (int i = 1; i <= m; i++) {
-    dp[i][0] = i;
+  for (int j = 1; j < n; j++) {
+    dp[0][j] = (s[0] == t[j]) ? j : (dp[0][j-1] + 1);
   }
 
-  for (int i = 1; i <= m; i++) {
-    for (int j = 1; j <= n; j++) {
-      // 推导dp[i][j]
-      dp[i][j] = std::min({
-        dp[i-1][j-1] + (p[i-1]==s[j-1] ? 0 : 1),  // 修改字符. p[i-1]变成s[j-1]
-        dp[i-1][j] + 1,  // 加入字符p[j-1]. p[0,i-2] s[0,j-1] => p[0,i-1] s[0,j-1]
-        dp[i][j-1] + 1,  // 加入字符s[j-1]. p[0,i-1] s[0,j-2] => p[0,i-1] s[0,j-1]
-      });
+  for (int i = 1; i < m; i++) {
+    for (int j = 1; j < n; j++) {
+      dp[i][j] = (s[i] == t[j]) ? dp[i-1][j-1] : (min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1])) + 1);
     }
   }
-  return dp[m][n];
+  return dp[m-1][n-1];
 }
